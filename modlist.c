a2b228b8c39d008c77e85569670566fa5b67ea87
Pabluskagamo <pabluskagamo@gmail.com>
2022-09-27 14:07:42 +0200

    Read

--- modlist.c
+++ modlist.c
@@ -2,16 +2,13 @@
 #include <linux/kernel.h>	/* Definición de KERN_INFO */
 #include <linux/slab.h>
 #include <linux/proc_fs.h>
-#include <linux/vmalloc.h>
+#include <linux/list.h>
 
 MODULE_LICENSE("GPL"); 	/*  Licencia del modulo */
 
+#define MAX_K       PAGE_SIZE
 
-//Reserva de memoria
-void *kmalloc( size_t size, gfp_t flags);
-void kfree( void *addr );
 
-#define MAX_K       4096
 //Entradas en /proc
 static struct proc_dir_entry *proc_entry;
 
@@ -23,139 +20,37 @@ struct list_item{
 
 struct list_head head;
 
-struct list_head {
-	struct list_head *next, *prev;
-};
-
-#define LIST_HEAD_INIT(name) { &(name), &(name) }
-
-#define LIST_HEAD(name) \
-	struct list_head name = LIST_HEAD_INIT(name)
-
-#define INIT_LIST_HEAD(ptr) do { \
-	(ptr)->next = (ptr); (ptr)->prev = (ptr); \
-} while (0)
-/*
- * These are non-NULL pointers that will result in page faults
- * under normal circumstances, used to verify that nobody uses
- * non-initialized list entries.
- */
-#define LIST_POISON1  ((void *) 0x00100100)
-#define LIST_POISON2  ((void *) 0x00200200)
-
-
-/*
- * Insert a new entry between two known consecutive entries.
- *
- * This is only for internal list manipulation where we know
- * the prev/next entries already!
- */
-static inline void __list_add(struct list_head *new,
-			      struct list_head *prev,
-			      struct list_head *next)
-{
-	next->prev = new;
-	new->next = next;
-	new->prev = prev;
-	prev->next = new;
-}
-
-/**
- * list_add - add a new entry
- * @new: new entry to be added
- * @head: list head to add it after
- *
- * Insert a new entry after the specified head.
- * This is good for implementing stacks.
- */
-static inline void list_add(struct list_head *new, struct list_head *head)
-{
-	__list_add(new, head, head->next);
-}
-
-/*
- * Delete a list entry by making the prev/next entries
- * point to each other.
- *
- * This is only for internal list manipulation where we know
- * the prev/next entries already!
- */
-static inline void __list_del(struct list_head * prev, struct list_head * next)
-{
-	next->prev = prev;
-	prev->next = next;
-}
-
-/**
- * list_del - deletes entry from list.
- * @entry: the element to delete from the list.
- * Note: list_empty on entry does not return true after this, the entry is
- * in an undefined state.
- */
-static inline void list_del(struct list_head *entry)
-{
-	__list_del(entry->prev, entry->next);
-	entry->next = LIST_POISON1;
-	entry->prev = LIST_POISON2;
-}
-
-/**
- * __list_for_each	-	iterate over a list
- * @pos:	the &struct list_head to use as a loop counter.
- * @head:	the head for your list.
- *
- * This variant differs from list_for_each() in that it's the
- * simplest possible list iteration code, no prefetching is done.
- * Use this for code that knows the list to be very short (empty
- * or 1 entry) most of the time.
- */
-#define __list_for_each(pos, head) \
-	for (pos = (head)->next; pos != (head); pos = pos->next)
-
-
-
-
-void print_list(struct list_head* list){
-    struct list_item* item = NULL;
-    struct list_head* cur_node = NULL;
-
-
-    __list_for_each(cur_node, list) {
-
-        item = list_entry(cur_node, struct list_item, links);
-        //Mostrar elemento en /proc
-    }
-
-}
-
-
 static ssize_t myproc_read(struct file *filp, char __user *buf, size_t len, loff_t *off) {
   
-  char kbuf[MAX_K];
+	char kbuf[MAX_K];
+	struct list_item* item = NULL;
+	struct list_head* cur_node = NULL;
+	
+	if ((*off) > 0) /* Tell the application that there is nothing left to read */
+		return 0;
 
-  int nr_bytes;
-  char *wrptr = kbuf;
-  int n;
- 
-  if ((*off) > 0) /* Tell the application that there is nothing left to read */
-      return 0;
+	char *wrptr = kbuf;
+	int nr_bytes = 0;
 
-  wrptr += sprintf(wrptr, "Hola soy pablo");
+	list_for_each(cur_node, &head) {
+		item = list_entry(cur_node, struct list_item, links);
 
-  printk(KERN_INFO "Imprim wrp%i\n",wrptr);
-  printk(KERN_INFO "Imprim wrp%i\n",len);
-    
-  if (len<wrptr)			
-    return -ENOSPC;
+		int n_bytes = sprintf(wrptr, "%d\n", item->data);
+		wrptr += n_bytes;
+		nr_bytes += n_bytes;
+	}
+		
+	if (len < nr_bytes)			
+		return -ENOSPC;
 
-  
-    /* Transfer data from the kernel to userspace */  
-  if (copy_to_user(buf, kbuf,nr_bytes))
-    return -EINVAL;
-    
-  (*off)+=len;  /* Update the file pointer */
+	/* Transfer data from the kernel to userspace */  
+	if (copy_to_user(buf, kbuf, nr_bytes))
+		return -EINVAL;
+	
 
-  return wrptr; 
+	(*off)+=len;  /* Update the file pointer */
+
+	return nr_bytes; 
 }
 
 
@@ -173,28 +68,15 @@ static ssize_t myproc_write(struct file *filp, const char __user *buf, size_t le
 		return -ENOSPC;
 	}
 
-	/* Transfer data from user to kernel space */
-	if (copy_from_user( &kbuf[0], buf, len ))  
-		return -EFAULT;
+	struct list_item *newNode;
 
-	if(sscanf(kbuf, "add %i", &n) == 1){
-		//meter n en la lista
-		printk(KERN_INFO "add\n");
-		list_head newNode * = kmalloc(sizeof(list_head), GFP_KERNEL);
-		
-		list_add(newNode, head);
-		printk(KERN_INFO "%i", head->);
-	}
-	else if(sscanf(kbuf, "remove %i", &n) == 1){
-		//eliminar de la lista
-		printk(KERN_INFO "remove\n");
-	}
-	// else if(sscanf(kbuf, "cleanup") == 1){
-	// 	printk(KERN_INFO "cleanup\n");
-	// }
-	else{
-		return -EINVAL;
-	}
+
+	newNode = kmalloc(sizeof(struct list_item), GFP_KERNEL);
+	newNode->data = 25;
+
+	list_add_tail(&newNode->links, &head);
+
+	printk(KERN_INFO "len:", "%d", len);
 
 	*off+=len;  
 	
@@ -219,6 +101,25 @@ int modulo_lin_init(void)
       printk(KERN_INFO "Modlist: Module loaded\n");
     }
 
+	INIT_LIST_HEAD(&head);
+
+	struct list_item *newNode;
+
+	newNode = kmalloc(sizeof(struct list_item), GFP_KERNEL);
+	newNode->data = 25;
+
+	list_add_tail(&newNode->links, &head);
+
+	newNode = kmalloc(sizeof(struct list_item), GFP_KERNEL);
+	newNode->data = 29;
+
+	list_add_tail(&newNode->links, &head);
+
+	newNode = kmalloc(sizeof(struct list_item), GFP_KERNEL);
+	newNode->data = 566;
+
+	list_add_tail(&newNode->links, &head);
+
 	/* Devolver 0 para indicar una carga correcta del módulo */
 	return 0;
 }
@@ -226,7 +127,7 @@ int modulo_lin_init(void)
 /* Función que se invoca cuando se descarga el módulo del kernel */
 void modulo_lin_clean(void)
 {
-	remove_proc_entry("clipboard", NULL);
+	remove_proc_entry("modlist", NULL);
 	printk(KERN_INFO "Modulo LIN descargado. Adios kernel.\n");
 }
 
