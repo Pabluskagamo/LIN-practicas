9458008a177cf8976035418c04dc4218bd38ecbf
Pablo Gamo González <85303911+Pabluskagamo@users.noreply.github.com>
2022-11-10 15:43:03 +0100

    Add files via upload

---
+++ Practica4/ParteA/prueba.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+while true;
+do
+	echo add 2 > /proc/modlist
+	echo Escribo 2
+	sleep 1
+done
---
+++ Practica4/ParteA/prueba1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+while true;
+do
+	echo add 1 > /proc/modlist
+	echo Escribo 1
+	sleep 1
+done
